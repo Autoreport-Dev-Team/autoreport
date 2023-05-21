0a0dbff390912d8622434a587f2bde6242e11277
ploqop <ploqop@gmail.com>
2023-05-21 16:57:41 +0300

    правки

--- autoreport.py
+++ autoreport.py
@@ -1,4 +1,4 @@
 from flask import Blueprint, Flask
 
-autoreport = Blueprint('autoreport', __name__, template_folder='templates', static_folder='static')
+# autoreport = Blueprint('autoreport', __name__, template_folder='templates', static_folder='static')
 app = Flask(__name__, template_folder='templates/autoreport')
